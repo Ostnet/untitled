b75f8c07baa0ec48422f416c08b6debc517345ba
OSTNAME <ostavnow.igor.ostnet@gmail.com>
2021-04-14 10:38:05 +0300

    222222

--- src/com/ostnet/Main.java
+++ src/com/ostnet/Main.java
@@ -1,8 +1,9 @@
-package com.ostname;
+package com.ostnet;
 
 public class Main {
 
     public static void main(String[] args) {
         // write your code here
+        System.out.println();
     }
 }
